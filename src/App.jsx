9c2fce6712c74621cc56b18cd003fb501046cf5b
elvisscochito <elvirodominguezsoriano@outlook.com>
2026-08-24 07:59:43 -0600

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import { Analytics } from '@vercel/analytics/next'
 import { useState } from 'react'
+import './App.css'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Analytics />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
